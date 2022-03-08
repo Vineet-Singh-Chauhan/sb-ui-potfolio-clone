b37e4af3d042de478f13e8f3a3541c5a57ec2b9b
Vineet-Singh-Chauhan <94364597+Vineet-Singh-Chauhan@users.noreply.github.com>
2022-03-08 10:53:12 +0530

    toggle menu added

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,8 +1,37 @@
-// function classToggle() {
-//     const navs = document.querySelectorAll('.navbar li')
+
+//--------------------navbar----------------------------------//
+
+let toggleBtn=document.querySelector(".tBtn");
+toggleBtn.addEventListener("click",toggle);
+let tVar=false;
+
+ function show(){
+    let menu=document.querySelectorAll(".navbar li");
+   //  console.log(menu)
     
-//     navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
-//   }
-  
-//   document.querySelector('.Navbar__Link-toggle')
-//     .addEventListener('click', classToggle);
+    menu.forEach(function(element){
+       element.style.display="block";
+    })
+}
+
+function hide(){
+    let menu=document.querySelectorAll(".navbar li");
+   //  console.log(menu)
+    
+    menu.forEach(function(element){
+       element.style.display="none";
+    })
+}
+
+function toggle(){
+if(tVar===false){
+    show();
+    tVar=true;
+}
+else if(tVar===true){
+    hide();
+    tVar=false
+}
+console.log(tVar)
+
+}
